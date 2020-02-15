dfffa6d1a68e0d4a358b117819449bb262265ad2
Dean Calver <me@deanoc.com>
2020-02-15 17:16:38 +0200

    exporing cuda exporting

--- compute_cuda/kernels/test.cu
+++ compute_cuda/kernels/test.cu
@@ -1,6 +1,16 @@
 // License Summary: MIT see LICENSE file
 
-__global__ void vectorAdd(const float *A, const float *B, float* C) {
+#include <cooperative_groups.h>
+
+namespace cg = cooperative_groups;
+
+__device__ float const * A;
+__device__ float const * B;
+__device__ float  * C;
+
+extern "C" __global__ void vectorAdd() {
+	cg::thread_block block = cg::this_thread_block();
+
 	int i = blockDim.x * blockIdx.x + threadIdx.x;
 	C[i] = A[i] + B[i];
 }
